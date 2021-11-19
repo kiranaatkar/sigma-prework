6b97d4bd036947e3790ed6bebb7338c21d206c01
Kiran Aatkar <kiranaatkar@gmail.com>
2021-11-19 21:29:03 +0000

    Created files for task 1 & 2, with the description of what is needed

---
+++ task_1.js
@@ -0,0 +1,16 @@
+/*
+Write a short program that, when given a date, calculates the age between the current date and the given date. 
+
+An example of an input and output might look like this: 
+
+1990-01-01 should return 31
+
+1972-12-04 should return 48 (this is the date format required to use Date.parse())
+
+
+Hints: you’ll likely need to use the JavaScript Date object. Some helpful links can be found here:
+
+Official Date documentation	
+
+W3Schools Date documentation
+*/
---
+++ task_2.js
@@ -0,0 +1,10 @@
+/*
+Given an array of integers, return the highest and lowest numbers in the array (without using Math.max() or Math.min())
+
+
+Some example inputs and outputs would look like the below:
+
+[2, 4, 1, 0, 2, -1] should return [-1, 4]
+
+[20, 50, 12, 6, 14, 8] should return [6, 50]
+*/
